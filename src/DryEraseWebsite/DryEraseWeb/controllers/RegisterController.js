500713eaedc224e300800d371796c1c05d23a017
somya <somy@s3mobileapps.com>
2013-03-05 11:49:12 -0600

    added brower action plugin

---
+++ src/DryEraseWebsite/DryEraseWeb/controllers/RegisterController.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+
+urbansafari.controller('RegisterController', function RegisterController($scope, $http, $cookies, $location, $routeParams) {
+	
+
+    $scope.FullName = null;
+    $scope.registerUser = function () {
+        AuthenticationService.register($scope.FullName, $scope.Email, $scope.Password).then(function (loggedIn) {
+            if (!loggedIn) {
+                $scope.authError = "Login failed.  Please check your credentials and try again.";
+            }
+            else {
+                $('.dropdown.open').removeClass('open');
+                $location.path('/event');
+            }
+        });
+    }
+	
+});

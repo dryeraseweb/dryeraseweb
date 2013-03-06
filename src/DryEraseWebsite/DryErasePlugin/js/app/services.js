932be2b476be3bae437523ffe359807e9fef6c4a
somya <somy@s3mobileapps.com>
2013-03-05 19:55:17 -0600

    removed old plugins

--- src/DryEraseWebsite/DryErasePlugin/js/app/services.js
+++
@@ -1,6 +0,0 @@
-﻿angular.module('DryEraseServices', ['ngResource']).
-    factory('Whiteboard', function($resource) {
-        return $resource('https://DryEraseWeb-Dev/api/whiteboard/:url', {url:'@url'}, {
-            get: { method: 'GET', isArray: false }
-        });
-    });

185c2c53013ea0b2a049cc4059cfc4a57a061f16
somya <somy@s3mobileapps.com>
2013-03-06 07:15:06 -0600

    fixed for badge

--- src/DryEraseWebsite/DryEraseWeb/plugin/js/app/content.js
+++ src/DryEraseWebsite/DryEraseWeb/plugin/js/app/content.js
@@ -3,9 +3,10 @@
 var DryEraseService = {
     load: function () {
 //        alert(window.location);
-        $.post('http://DryEraseWeb-Dev/api/itemcount?format=json', { Url: window.location.href}, function(data) {
-          
-            chrome.extension.sendMessage({ type: "badge", count: data.toString() }, function(response) {
+        $.post('http://DryEraseWeb-Dev/api/itemcount?format=json', { "Url": window.location.href}, function(data) {
+
+            var badge = data > 0 ? data.toString() : "";
+            chrome.extension.sendMessage({ type: "badge", count: badge }, function(response) {
             });
         })
             
